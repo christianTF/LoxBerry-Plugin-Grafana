7b67cecc4fe331bc96d78077249ee6dc51fdfbad
Christian Fenzl <fenzl@t-r-t.at>
2021-03-09 18:57:44 +0100

    Use Grafana repo for installation
    - Use Grafana repo for installation
    - Rename plugin
    - Rename author
    - Version numbering by date

--- postroot.sh
+++ postroot.sh
@@ -4,24 +4,18 @@
 LBPCONFIGDIR=REPLACELBPCONFIGDIR
 LBPDATADIR=REPLACELBPDATADIR
 
-DOWNLOADDIR=$LBPDATADIR/download
 
-mkdir $DOWNLOADDIR
-chown loxberry:loxberry $DOWNLOADDIR
-rm -f $DOWNLOADDIR/*
 
-# Downloading Grafana
-wget -P $DOWNLOADDIR https://s3-us-west-2.amazonaws.com/grafana-releases/release/grafana_5.2.2_armhf.deb 
-chown -f loxberry:loxberry $DOWNLOADDIR/* 
+
 
 # Installing
-dpkg -i $DOWNLOADDIR/grafana_5.2.2_armhf.deb
+#dpkg -i $DOWNLOADDIR/grafana_5.2.2_armhf.deb
 
-if [ $? -ne 0 ]
-then
-	echo "Installation of Grafana failed - Terminating"
-	exit 1
-fi
+#if [ $? -ne 0 ]
+#then
+#	echo "Installation of Grafana failed - Terminating"
+#	exit 1
+#fi
 
 # Reload daemons and auto-start grafana on boot
 systemctl daemon-reload
@@ -34,17 +28,17 @@ systemctl enable grafana-server
 # fi
 
 # Install Grafana Simple-JSON Plugin
-/usr/sbin/grafana-cli plugins install grafana-simple-json-datasource
+# /usr/sbin/grafana-cli plugins install grafana-simple-json-datasource
 
 # Provisioning Stats4Lox (SimpleJson) datasource
-cp -r -f $LBPDATADIR/shipment/provisioning/* /etc/grafana/provisioning/
-chown -R grafana:grafana /etc/grafana/provisioning/
+# cp -r -f $LBPDATADIR/shipment/provisioning/* /etc/grafana/provisioning/
+# chown -R grafana:grafana /etc/grafana/provisioning/
 
 # # chown
 # chown -R loxberry:loxberry $LBPCONFIGDIR
 # chmod -R 775 $LBPCONFIGDIR
 
-usermod -a -G loxberry grafana
+# usermod -a -G loxberry grafana
 
 # Grafana starten/restarten
 systemctl restart grafana-server
--- preroot.sh
+++ preroot.sh
@@ -4,4 +4,11 @@
 # Grafana stoppen
 systemctl stop grafana-server
 
+echo "<INFO> Adding/Updating Grafana repository..."
+echo "deb https://packages.grafana.com/oss/deb stable main" | sudo tee /etc/apt/sources.list.d/grafana.list
+
+echo "<INFO> Adding/Updating repository key to apt..."
+wget -q -O - https://packages.grafana.com/gpg.key | sudo apt-key add -
+
+
 exit 0
