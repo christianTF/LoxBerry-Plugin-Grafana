1c1139c2a7c19254d513a0fd01c0bf5bd6fb5788
Christian Fenzl <fenzl@t-r-t.at>
2021-03-09 19:17:11 +0100

    preroot: Remove apt-key warning

--- preroot.sh
+++ preroot.sh
@@ -8,7 +8,7 @@ echo "<INFO> Adding/Updating Grafana repository..."
 echo "deb https://packages.grafana.com/oss/deb stable main" | sudo tee /etc/apt/sources.list.d/grafana.list
 
 echo "<INFO> Adding/Updating repository key to apt..."
-wget -q -O - https://packages.grafana.com/gpg.key | sudo apt-key add -
+wget -q -O - https://packages.grafana.com/gpg.key | sudo apt-key add - 2>/dev/null
 
 echo "<INFO> Updating apt database..."
 apt-get -q -y --allow-unauthenticated --allow-downgrades --allow-remove-essential --allow-change-held-packages update
