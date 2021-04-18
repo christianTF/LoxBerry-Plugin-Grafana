1e0fc1a78827cac9eeedde5c89bc330ae81436b7
Christian Fenzl <fenzl@t-r-t.at>
2021-04-18 09:30:40 +0200

    postroot: Set S4L Provisioning on install

--- postroot.sh
+++ postroot.sh
@@ -1,12 +1,24 @@
 #!/bin/bash
 # Will be executed as user "root".
 
-LBPCONFIGDIR=REPLACELBPCONFIGDIR
-LBPDATADIR=REPLACELBPDATADIR
+# To use important variables from command line use the following code:
+COMMAND=$0    # Zero argument is shell command
+PTEMPDIR=$1   # First argument is temp folder during install
+PSHNAME=$2    # Second argument is Plugin-Name for scipts etc.
+PDIR=$3       # Third argument is Plugin installation folder
+PVERSION=$4   # Forth argument is Plugin version
+#LBHOMEDIR=$5 # Comes from /etc/environment now. Fifth argument is
+              # Base folder of LoxBerry
+PTEMPPATH=$6  # Sixth argument is full temp path during install (see also $1)
 
-
-
-
+# Combine them with /etc/environment
+LBPHTMLAUTHDIR=$LBPHTMLAUTH/$PDIR
+LBPHTMLDIR=$LBPHTML/$PDIR
+LBPTEMPLATEDIR=$LBPTEMPL/$PDIR
+LBPDATADIR=$LBPDATA/$PDIR
+LBPLOGDIR=$LBPLOG/$PDIR # Note! This is stored on a Ramdisk now!
+LBPCONFIGDIR=$LBPCONFIG/$PDIR
+LBPBINDIR=$LBPBIN/$PDIR
 
 # Installing
 #dpkg -i $DOWNLOADDIR/grafana_5.2.2_armhf.deb
@@ -38,7 +50,12 @@ systemctl enable grafana-server
 # chown -R loxberry:loxberry $LBPCONFIGDIR
 # chmod -R 775 $LBPCONFIGDIR
 
-# usermod -a -G loxberry grafana
+usermod -a -G loxberry grafana
+
+if [ -e "$LBPBIN/stats4lox-ng/provisioning/set_datasource_influx.pl" ]; then
+	$LBPBIN/stats4lox-ng/provisioning/set_datasource_influx.pl
+	$LBPBIN/stats4lox-ng/provisioning/set_dashboard_provider.pl
+fi
 
 # Grafana starten/restarten
 systemctl restart grafana-server
